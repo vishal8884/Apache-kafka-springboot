d2f23a1249a5c1a7ce666c90d71bcf1b521c6dc1
vishal <vishalambekar459@gmail.com>
2023-10-30 19:30:24 +0530

    real world kafka app created

---
+++ Spring-boot-kafka-real-word/Spring-boot-kafka-real-word/src/main/java/com/vishal/kafka/Springbootkafkarealword/SpringBootKafkaRealWordApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.kafka.Springbootkafkarealword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaRealWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaRealWordApplication.class, args);
+	}
+
+}
---
+++ Spring-boot-kafka-real-word/Spring-boot-kafka-real-word/src/test/java/com/vishal/kafka/Springbootkafkarealword/SpringBootKafkaRealWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.kafka.Springbootkafkarealword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaRealWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
